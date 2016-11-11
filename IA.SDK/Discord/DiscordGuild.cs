e0ded404bc1b005d3c4e96ef3eced5bbc0cd4fcc
M. Veldsink <velddev@gmail.com>
2016-11-11 02:33:23 +0100

    Create: Open source SDK repository

---
+++ IA.SDK/Discord/DiscordGuild.cs
@@ -0,0 +1,20 @@
+﻿using IA.SDK.Interfaces;
+
+namespace IA.SDK
+{
+    public class DiscordGuild : IDiscordGuild
+    {
+        public ulong Id
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public virtual DiscordChannel GetDefaultChannel()
+        {
+            return null;
+        }
+    }
+}
---
+++ IA.SDK/ModuleData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace IA.SDK
+{
+    public class ModuleData
+    {
+        public string name;
+        public bool enabled;
+
+        public List<CommandEvent> events = new List<CommandEvent>();
+    }
+}
