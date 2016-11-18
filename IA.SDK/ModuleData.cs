a2ca4ef36c3b680b67aaabb510d4668a728b5a2e
Mike Veldsink <veldsinkie.mv@gmail.com>
2016-11-18 22:43:53 +0100

    Add: Module events

--- IA.SDK/ModuleData.cs
+++ IA.SDK/ModuleData.cs
@@ -1,12 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using IA.SDK.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace IA.SDK
 {
+    public delegate Task MessageRecievedEventDelegate(IDiscordMessage msg);
+    public delegate Task UserUpdatedEventDelegate(IDiscordUser oldUser, IDiscordUser newUser);
+
     public class ModuleData
     {
         public string name;
         public bool enabled;
 
         public List<CommandEvent> events = new List<CommandEvent>();
+
+        public MessageRecievedEventDelegate messageRecieved;
+        public UserUpdatedEventDelegate userUpdated;
     }
 }
