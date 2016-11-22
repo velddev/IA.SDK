08c773f4ccbd9fa3cc466995695b9ac3dc09a03f
M. Veldsink <velddev@gmail.com>
2016-11-22 22:00:55 +0100

    Add: Experimental(!!) Audio Support

---
+++ IA.SDK/Interfaces/IAudio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA.SDK.Interfaces
+{
+    interface IAudio
+    {
+        Stream AudioBytes { get; set; }
+    }
+}
---
+++ IA.SDK/Interfaces/IDiscordAudioClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA.SDK.Interfaces
+{
+    interface IDiscordAudioClient
+    {
+        bool IsPlaying { get; }
+
+        Task Connect();
+        Task Disconnect();
+
+        Task Play(IAudio audio);
+        Task Pause();
+    }
+}
