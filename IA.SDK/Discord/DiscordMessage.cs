4fb2db73a3fd6aa624db2c6a1d1e3534b53605da
Mike <veldsinkie.mv@gmail.com>
2017-03-26 23:58:55 +0200

    Clean: DiscordMessage.cs

--- IA.SDK/Discord/DiscordMessage.cs
+++ IA.SDK/Discord/DiscordMessage.cs
@@ -1,143 +1,38 @@
-﻿using IA.SDK.Interfaces;
+﻿#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
+using IA.SDK.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace IA.SDK
 {
+    // TODO: Clean
     public class DiscordMessage : IDiscordMessage
     {
-        public virtual ulong Id
-        {
-            get
-            {
-                return 0;
-            }
-        }
+        public virtual ulong Id => 0;
+        public virtual IDiscordUser Author => throw new NotImplementedException();
+        public virtual IDiscordUser Bot => throw new NotImplementedException();
+        public virtual IDiscordGuild Guild => throw new NotImplementedException();
+        public virtual string Content => throw new NotImplementedException();
+        public virtual DateTimeOffset Timestamp => throw new NotImplementedException();
+        public virtual IReadOnlyCollection<ulong> MentionedUserIds => throw new NotImplementedException();
+        public virtual IReadOnlyCollection<ulong> MentionedRoleIds => throw new NotImplementedException();
+        public int ShardId => throw new NotImplementedException();
+        public IDiscordClient Discord => throw new NotImplementedException();
+        private IDiscordMessageChannel Channel => throw new NotImplementedException();
+        public IDiscordAudioChannel VoiceChannel => throw new NotImplementedException();
+        IDiscordMessageChannel IDiscordMessage.Channel => throw new NotImplementedException();
+        public Dictionary<DiscordEmoji, int> Reactions => throw new NotImplementedException();
 
-        public virtual IDiscordUser Author
-        {
-            get
-            {
-                return null;
-            }
-        }
+        public virtual async Task DeleteAsync() => throw new NotImplementedException();
 
-        public virtual IDiscordUser Bot
-        {
-            get
-            {
-                return null;
-            }
-        }
+        public virtual async Task ModifyAsync(string message) => throw new NotImplementedException();
 
-        public virtual IDiscordGuild Guild
-        {
-            get
-            {
-                return null;
-            }
-        }
+        public virtual async Task PinAsync() => throw new NotImplementedException();
 
-        public virtual string Content
-        {
-            get
-            {
-                return "";
-            }
-        }
+        public virtual async Task UnpinAsync() => throw new NotImplementedException();
 
-        public virtual DateTimeOffset Timestamp
-        {
-            get
-            {
-                return new DateTime(0);
-            }
-        }
-
-        public virtual IReadOnlyCollection<ulong> MentionedUserIds
-        {
-            get
-            {
-                return null;
-            }
-        }
-
-        public virtual IReadOnlyCollection<ulong> MentionedRoleIds
-        {
-            get
-            {
-                return null;
-            }
-        }
-
-        public int ShardId
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public IDiscordClient Discord
-        {
-            get
-            {
-                return null;
-            }
-        }
-
-        private IDiscordMessageChannel Channel
-        {
-            get
-            {
-                return null;
-            }
-        }
-
-        public IDiscordAudioChannel VoiceChannel
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        IDiscordMessageChannel IDiscordMessage.Channel
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public virtual async Task DeleteAsync()
-        {
-            await Task.CompletedTask;
-            throw new NotImplementedException();
-        }
-
-        public virtual async Task ModifyAsync(string message)
-        {
-            await Task.CompletedTask;
-            throw new NotImplementedException();
-        }
-
-        public virtual async Task PinAsync()
-        {
-            await Task.CompletedTask;
-            throw new NotImplementedException();
-        }
-
-        public virtual async Task UnpinAsync()
-        {
-            await Task.CompletedTask;
-            throw new NotImplementedException();
-        }
-
-        public virtual IDiscordEmbed CreateEmbed()
-        {
-            throw new NotImplementedException();
-        }
+        public virtual IDiscordEmbed CreateEmbed() => throw new NotImplementedException();
     }
 }
