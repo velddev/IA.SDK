23b9bdafc3ca2f861d4e4c3eb90996dfb24864a8
Veld <velddev@gmail.com>
2017-06-02 15:09:31 +0200

    Set: canbedisabled default to true

--- IA.SDK/Events/Event.cs
+++ IA.SDK/Events/Event.cs
@@ -15,7 +15,7 @@ namespace IA.SDK.Events
         public EventMetadata Metadata { get; set; } = new EventMetadata();
 
         public bool OverridableByDefaultPrefix { get; set; } = false;
-        public bool CanBeDisabled { get; set; } = false;
+        public bool CanBeDisabled { get; set; } = true;
         public bool DefaultEnabled { get; set; } = true;
 
         public IModule Module { get; set; }
